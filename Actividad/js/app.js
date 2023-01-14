5519fda13774d28a0c2739f0d4790b7a258fd00a
Jorge Antonio Monge Aquino <41486188+JorgeMge@users.noreply.github.com>
2023-01-13 22:54:55 -0600

    Lineas
    Se agregaron las lineas

--- Actividad/js/app.js
+++ Actividad/js/app.js
@@ -5,55 +5,37 @@ const draw = async (el = "#graf") => {
 
   // Carga del dataset
   let dataset = await d3.csv("csv/vehiculos.csv", d3.autoType)
-  //dataset = dataset.filter((d) => d.income > 0 && d.life_exp > 0)
 
   // Dimensiones
   const anchoTotal = +graf.style("width").slice(0, -2)
   const altoTotal = anchoTotal * 0.38
 
-  const margins = { top: 20, right: 60, bottom: 75, left: 120 }
+  const margins = { top: 20, right: 20, bottom: 75, left: 120 }
 
-  const alto = altoTotal - margins.top - margins.bottom
+  const alto = altoTotal - margins.top - margins.bottom 
   const ancho = anchoTotal - margins.left - margins.right
 
   // Accessors
-  const xAccessor = (d) => d.mesid
+  const xAccessor = (d) => d.anio
   const yAccessor = (d) => d.valor
   //const rAccessor = (d) => d.population
 
   // Tipo
   const tipo = Array.from(new Set(dataset.map((d) => d.tipo)))
-  /*const selcon = d3.select("#continent")
-
-  let cont = ["todos"]
-  continents.forEach((c) => {
-    cont.push(c)
-  })
-
-  selcon
-    .selectAll("option")
-    .data(cont)
-    .join("option")
-    .attr("value", (d) => d)
-    .text((d) => d)*/
 
   // Escaladores
-  const color = d3.scaleOrdinal().domain(tipo).range(d3.schemeTableau10)
+  const color = d3.scaleOrdinal().domain(tipo).range(d3.schemeSet1)
   const x = d3
     //.scaleLog()
     .scaleLinear()
     .domain(d3.extent(dataset, xAccessor))
-    .range([0, ancho * 1.1])
+    .range([0, ancho])
+
   const y = d3
     .scaleLinear()
     .domain(d3.extent(dataset, yAccessor))
     .range([alto, 0])
     .nice()
-  /*const r = d3
-    .scaleLinear()
-    .domain(d3.extent(dataset, rAccessor))
-    .range([25, 25000])
-    .nice()*/
 
   // Espacio de gráfica
   const svg = graf
@@ -66,7 +48,7 @@ const draw = async (el = "#graf") => {
     .append("g")
     .append("rect")
     .attr("transform", `translate(${margins.left}, ${margins.top})`)
-    .attr("width", ancho * 1.05)
+    .attr("width", ancho)
     .attr("height", alto)
     .attr("class", "backdrop")
 
@@ -74,7 +56,7 @@ const draw = async (el = "#graf") => {
     .append("clipPath")
     .attr("id", "clip")
     .append("rect")
-    .attr("width", ancho * 1.05)
+    .attr("width", ancho)
     .attr("height", alto)
 
   const chart = svg
@@ -82,16 +64,19 @@ const draw = async (el = "#graf") => {
     .attr("transform", `translate(${margins.left}, ${margins.top})`)
 
   // Variables
-  const yearMin = d3.min(dataset, (d) => d.mesid)
-  const yearMax = d3.max(dataset, (d) => d.mesid)
-  //let mes = "ENERO"//Math.floor((yearMax - yearMin) / 2) + yearMin
+  const yearMin = d3.min(dataset, (d) => d.anio)
+  const yearMax = d3.max(dataset, (d) => d.anio)
+  let ban = 1   // Bandera para crear las lineas en la primera iteración
+  let linesi    
+  let linese    
+  // Arreglo de meses para mostrar el nombre
   const meses = ['ENERO', 'FEBRERO', 'MARZO', 'ABRIL', 'MAYO', 'JUNIO', 'JULIO', 'AGOSTO', 'SEPTIEMBRE', 'OCTUBRE', 'NOVIEMBRE', 'DICIEMBRE'];
-  let mes = 0
+  let mes = 0   // Mes incial
   let mesInterval
   let running = false
   const play = d3.select("#play")
-  //let filtroContinente = "todos"
 
+  //Texto de mes
   const mesLayer = chart
     .append("g")
     .append("text")
@@ -100,27 +85,75 @@ const draw = async (el = "#graf") => {
     .classed("mes", true)
     .text(meses[mes])
 
-  const step = (year) => {
-    let newDataset = dataset.filter((d) => d.mes == mes+1)
-
-    /*if (filtroContinente != "todos") {
-      newDataset = newDataset.filter((d) => d.tipo == filtroContinente)
-    }*/
+  const step = (mes) => {
+    //Filtrado
+    let ds = dataset.filter((d) => d.mes == mes+1)
+    let dsi = ds.filter((d) => d.tipo == "Importaciones")
+    let dse = ds.filter((d) => d.tipo == "Exportaciones")
 
     // Dibujar los puntos
     const circles = chart
       .selectAll("circle")
-      .data(newDataset)
-      //.data(newDataset)
+      .data(ds)
       .join("circle")
+      .transition() 
+      .duration(1000)
       .attr("cx", (d) => x(xAccessor(d)))
       .attr("cy", (d) => y(yAccessor(d)))
-      .attr("r", (d) => 4)
-      //.attr("r", (d) => Math.sqrt(r(rAccessor(d) / Math.PI)))
+      .attr("r", (d) => 7)
       .attr("fill", (d) => color(d.tipo))
       .attr("stroke", "#999")
-      .attr("opacity", 0.6)
       .attr("clip-path", "url(#clip)")
+
+    //Dibujar líneas
+    if(ban==1){
+      ban=0;
+      linesi = chart
+      .append("path")
+      .attr("class","linei")
+      .datum(dsi)
+      .transition() 
+      .duration(1000)
+      .attr("fill", "none")
+      .attr("stroke", (d) => color(d.tipo))
+      .attr("stroke-width", 1)
+      .attr("d", d3.line()
+          .x((d) => x(d.anio))
+          .y((d) => y(d.valor))
+        );
+
+      linese = chart
+      .append("path")
+      .attr("class","linee")
+      .datum(dse)
+      .transition() 
+      .duration(1000)
+      .attr("fill", "none")
+      .attr("stroke", (d) => color(d.tipo))
+      .attr("stroke-width", 1)
+      .attr("d", d3.line()
+          .x((d) => x(d.anio))
+          .y((d) => y(d.valor))
+        );
+    }else{
+      chart.select("path.linei") // selecciona la línea existente en el gráfico
+        .datum(dsi) // reemplaza los datos asociados con los nuevos datos
+        .transition() 
+        .duration(1000) // opcional, si quieres mantener la animación
+        .attr("d", d3.line()
+          .x((d) => x(d.anio))
+          .y((d) => y(d.valor))
+        );
+
+        chart.select("path.linee") // selecciona la línea existente en el gráfico
+        .datum(dse) // reemplaza los datos asociados con los nuevos datos
+        .transition() 
+        .duration(1000) // opcional, si quieres mantener la animación
+        .attr("d", d3.line()
+          .x((d) => x(d.anio))
+          .y((d) => y(d.valor))
+        );
+    }
 
     mesLayer.text(meses[mes])
   }
@@ -156,13 +189,8 @@ const draw = async (el = "#graf") => {
     running = !running
   })
 
-  /*selcon.on("change", () => {
-    filtroContinente = selcon.node().value
-    step(year)
-  })*/
-
   const FormatX = (d) => {
-    return d/100
+    return d
   }
 
   const FormatY = (d) => {
@@ -172,22 +200,24 @@ const draw = async (el = "#graf") => {
   // Ejes
   const xAxis = d3
     .axisBottom(x)
-    .ticks(10)
-    //.tickFormat((d) => d.toLocaleString())
+    .ticks(20)
     .tickSize(-alto)
     .tickFormat(FormatX)
 
   const yAxis = d3.axisLeft(y)
-    .tickSize(-ancho * 1.05)
+    .tickSize(-ancho)
     .tickFormat(FormatY)
                   
   const xAxisGroup = chart
     .append("g")
     .attr("transform", `translate(0, ${alto})`)
     .call(xAxis)
-    //.attr("transform", "rotate(-30)")
     .classed("axis", true)
-  const yAxisGroup = chart.append("g").call(yAxis).classed("axis", true)
+
+  const yAxisGroup = chart
+    .append("g")
+    .call(yAxis)
+    .classed("axis", true)
 
   xAxisGroup
     .append("text")
@@ -204,6 +234,7 @@ const draw = async (el = "#graf") => {
     .style("text-anchor", "middle")
     .style("transform", "rotate(270deg)")
     .text("Valor")
+
 }
 
 draw()
